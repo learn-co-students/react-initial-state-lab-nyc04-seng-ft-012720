649d61960573cd146ee83da82ef5a631239b3772
dagpan <panagiotisntagkinis@gmail.com>
2020-04-03 18:07:18 -0400

    Done
    All Tests Passing

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
-// your Bomb code here!
+import React from 'react';
+
+
+
+
+class Bomb extends React.Component {
+
+    state = {
+        secondsLeft: this.props.initialCount
+      }
+
+  
+    render() {
+      return (
+      <div>
+         { this.state.secondsLeft === 0 
+        ?
+        <h1>Boom!</h1>
+        :
+        <h1>{this.state.secondsLeft} seconds left before I go boom!</h1>
+         }
+       
+      </div>
+      )
+    }
+  }
+  
+  export default Bomb;
+  
